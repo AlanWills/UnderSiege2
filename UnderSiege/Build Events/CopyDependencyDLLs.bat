ac71d816e82854b31cf91b55a40b8f31d35a42a9
Alan Wills <alawills@googlemail.com>
2018-03-18 10:18:40 +0000

    Added submodule and updated build paths and build event scripts

--- UnderSiege/Build Events/CopyDependencyDLLs.bat
+++ UnderSiege/Build Events/CopyDependencyDLLs.bat
@@ -5,4 +5,4 @@ rem echo %OutputDir% > log.txt
 
 cd %OutputDir%
 
-(robocopy ..\..\..\..\..\CelesteEngine\CelesteEngine\bin\Win32\%Configuration%\ .\ /IS /IT /xo) & exit 0
+(robocopy ..\..\..\..\CelesteEngine\CelesteEngine\bin\Win32\%Configuration%\ .\ /IS /IT /xo) & exit 0
--- UnderSiegeBindingsGenerator/Build Events/CopyDependencyDLLs.bat
+++ UnderSiegeBindingsGenerator/Build Events/CopyDependencyDLLs.bat
@@ -6,5 +6,5 @@ rem echo %OutputDir% > log.txt
 
 cd %OutputDir%
 
-(robocopy ..\..\..\..\..\CelesteEngine\CelesteEngine\bin\Win32\%Configuration%\ .\ /IS /IT /xo) ^&
-(robocopy ..\..\..\..\..\CelesteEngine\BindingsGenerator\bin\Win32\%Configuration%\ .\ /IS /IT /xo) ^& exit 0
+(robocopy ..\..\..\..\CelesteEngine\CelesteEngine\bin\Win32\%Configuration%\ .\ /IS /IT /xo) ^&
+(robocopy ..\..\..\..\CelesteEngine\BindingsGenerator\bin\Win32\%Configuration%\ .\ /IS /IT /xo) ^& exit 0
