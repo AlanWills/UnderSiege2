8637637e10a85cacba1317a9dbca234a3713d4b4
Alan Wills <alawills@googlemail.com>
2018-08-14 22:34:50 +0100

    Got all projects compiling under x64

--- UnderSiegeBindingsGenerator/Build Events/CopyDependencyDLLs.bat
+++ UnderSiegeBindingsGenerator/Build Events/CopyDependencyDLLs.bat
@@ -1,10 +1,11 @@
 set "OutputDir=%1"
 set "Configuration=%2"
+set "Platform=%3"
 
 rem Debugging
 rem echo %OutputDir% > log.txt
 
 cd %OutputDir%
 
-(robocopy ..\..\..\..\CelesteEngine\CelesteEngine\bin\Win32\%Configuration%\ .\ /IS /IT /xo) ^&
-(robocopy ..\..\..\..\CelesteEngine\BindingsGenerator\bin\Win32\%Configuration%\ .\ /IS /IT /xo) ^& exit 0
+(robocopy ..\..\..\..\CelesteEngine\CelesteEngine\bin\%Platform%\%Configuration%\ .\ /IS /IT /xo) ^&
+(robocopy ..\..\..\..\CelesteEngine\BindingsGenerator\bin\%Platform%\%Configuration%\ .\ /IS /IT /xo) ^& exit 0
