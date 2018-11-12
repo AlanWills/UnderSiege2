d76aa2ad6cbf56e2657b6f2da8575dfea01c927e
Alan Wills <alawills@googlemail.com>
2018-11-12 18:00:20 +0000

    Updating game script to use new game settings userdata

--- UnderSiege/Resources/Scripts/Game.lua
+++ UnderSiege/Resources/Scripts/Game.lua
@@ -2,13 +2,11 @@ package.path = package.path .. ";" .. getResourcesDirectory() .. "\\?.lua"
 local path = require "Scripts.Core.FileSystem.Path"
 
 local settingsPath = path.combine(getResourcesDirectory(), "Data", "Settings", "GameSettings.asset")
-local settings = loadScriptableObject("GameSettings", settingsPath);
+local settings = GameSettings.load(settingsPath)
 
 setMasterVolume(settings:getMasterVolume())
 setMusicVolume(settings:getMusicVolume())
 setSFXVolume(settings:getSFXVolume())
-
-deleteScriptableObject(settings)
 
 loadScreen(path.combine(getResourcesDirectory(), "Data", "Screens", "PersistentStartupAndMainMenu.asset"));
 loadScreen(path.combine(getResourcesDirectory(), "Data", "Screens", "SplashScreen.asset"));
