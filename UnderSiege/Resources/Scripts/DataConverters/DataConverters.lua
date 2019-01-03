9a74f09935642a46af4c26015ec20b8b4101b19f
Alan Wills <alawills@googlemail.com>
2019-01-03 00:29:34 +0000

    Rolling out new canvas renderer and added data converter

--- UnderSiege/Resources/Scripts/DataConverters/DataConverters.lua
+++ UnderSiege/Resources/Scripts/DataConverters/DataConverters.lua
@@ -1,4 +1,12 @@
+-- Animation
 require "Scripts.DataConverters.Animation.AnimatorDataConverter"
+
+-- Input
 require "Scripts.DataConverters.Input.KeyboardActivatorDataConverter"
 require "Scripts.DataConverters.Input.KeyboardVisibilityDataConverter"
+
+-- Physics
 require "Scripts.DataConverters.Physics.EllipseColliderDataConverter"
+
+-- Rendering
+require "Scripts.DataConverters.Rendering.CanvasRendererDataConverter"
