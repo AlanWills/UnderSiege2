dbd1863a5c6d0e794d8fb057caaf8dc2d898ab85
Alan Wills <alawills@googlemail.com>
2019-01-07 00:21:33 +0000

    Updating submodules

--- UnderSiegeEngine/Source/Turrets/AimControl/ManualAimController.cpp
+++ UnderSiegeEngine/Source/Turrets/AimControl/ManualAimController.cpp
@@ -30,11 +30,7 @@ namespace US
 
     const glm::vec3& worldTranslation = getTransform()->getWorldTranslation();
     glm::vec2 mouseScreenPosition = CelesteEngine::Input::getMouse().getScreenPosition();
-    //const glm::vec3& cameraWorldTranslation = getGameObject()->getOwnerScreen()->getWorldLayer().getCamera().getTransform()->getWorldTranslation();
-
-    float mouseWorldSpaceX = mouseScreenPosition.x; //+ cameraWorldTranslation.x;
-    float mouseWorldSpaceY = mouseScreenPosition.y; //+ cameraWorldTranslation.y;
-    float angle = Maths::lookAt(worldTranslation, glm::vec2(mouseWorldSpaceX, mouseWorldSpaceY));
+    float angle = Maths::lookAt(worldTranslation, glm::vec2(mouseScreenPosition.x, mouseScreenPosition.y));
 
     getTransform()->setWorldRotation(angle);
   }
