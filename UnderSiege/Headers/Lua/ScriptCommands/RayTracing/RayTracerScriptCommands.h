72f94809824b1d192bd011703ebe06d123119fdc
Alan Wills <alan@fuseboxgames.com>
2019-01-29 13:38:51 +0000

    Fixed linker error with ray tracer script commands - it was compiling it like a header

--- UnderSiege/Headers/Lua/ScriptCommands/RayTracing/RayTracerScriptCommands.h
+++ UnderSiege/Headers/Lua/ScriptCommands/RayTracing/RayTracerScriptCommands.h
@@ -13,29 +13,10 @@ namespace US
     {
       namespace Internals
       {
-        void addSphere(RayTracing::RayTracer& tracer, sol::table& table)
-        {
-          glm::vec3 position;
-          CelesteEngine::deserialize<glm::vec3>(table.get_or<std::string>("position", "0"), position);
-
-          float radius = table.get_or("radius", 1.0f);
-
-          glm::vec3 colour;
-          CelesteEngine::deserialize<glm::vec3>(table.get_or<std::string>("colour", "1"), colour);
-
-          tracer.getWorld()->addObject(new Sphere(Point3D(position.x, position.y, position.z), radius, RGBColor(colour.r, colour.g, colour.b)));
-        }
+        void addSphere(RayTracing::RayTracer& tracer, sol::table& table);
       }
 
-      void initialize()
-      {
-        sol::state& state = CelesteEngine::Lua::LuaState::instance();
-
-        state.new_usertype<RayTracing::RayTracer>(
-          "RayTracer",
-          "allocate", sol::factories(&RayTracing::RayTracer::allocate),
-          "addSphere", &Internals::addSphere);
-      }
-    };
+      void initialize();
+    }
   }
 }
--- UnderSiege/Source/Lua/ScriptCommands/RayTracing/RayTracerScriptCommands.cpp
+++ UnderSiege/Source/Lua/ScriptCommands/RayTracing/RayTracerScriptCommands.cpp
@@ -12,14 +12,22 @@ namespace US
       namespace Internals
       {
         //------------------------------------------------------------------------------------------------
-        /*void addSphere(RayTracing::RayTracer& tracer, sol::table& table)
+        void addSphere(RayTracing::RayTracer& tracer, sol::table& table)
         {
+          glm::vec3 position;
+          CelesteEngine::deserialize<glm::vec3>(table.get_or<std::string>("position", "0"), position);
 
-        }*/
+          float radius = table.get_or("radius", 1.0f);
+
+          glm::vec3 colour;
+          CelesteEngine::deserialize<glm::vec3>(table.get_or<std::string>("colour", "1"), colour);
+
+          tracer.getWorld()->addObject(new Sphere(Point3D(position.x, position.y, position.z), radius, RGBColor(colour.r, colour.g, colour.b)));
+        }
       }
 
       //------------------------------------------------------------------------------------------------
-      /*void initialize()
+      void initialize()
       {
         sol::state& state = CelesteEngine::Lua::LuaState::instance();
 
@@ -27,7 +35,7 @@ namespace US
           "RayTracer",
           "allocate", sol::factories(&RayTracing::RayTracer::allocate),
           "addSphere", &Internals::addSphere);
-      }*/
+      }
     }
   }
 }
